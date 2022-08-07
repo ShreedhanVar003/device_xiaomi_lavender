a09de97467aded5c19f6b4656f4f30ddf9e8d09b
Shreedhan003 <shridhan98@gmail.com>
2022-08-07 11:51:59 +0530

    lavender: props: Move vendor props to system
    [    3.123894] init: Do not have permissions to set 'persist.vendor.qti.telephony.vt_cam_interface' to '2' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.123938] init: Do not have permissions to set 'persist.radio.VT_CAM_INTERFACE' to '2' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124088] init: Do not have permissions to set 'ro.vendor.qti.sys.fw.bservice_enable' to 'true' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124200] init: Do not have permissions to set 'persist.device_config.runtime_native.usap_pool_enabled' to 'true' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124539] init: Do not have permissions to set 'ro.audio.soundfx.dirac' to 'true' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124582] init: Do not have permissions to set 'persist.audio.dirac.speaker' to 'true' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124601] init: Do not have permissions to set 'persist.dirac.acs.controller' to 'qem' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124618] init: Do not have permissions to set 'persist.dirac.acs.ignore_error' to '1' in property file '/vendor/build.prop': SELinux permission check failed
    [    3.124634] init: Do not have permissions to set 'persist.dirac.acs.storeSettings' to '1' in property file '/vendor/build.prop': SELinux permission check failed
    
    Signed-off-by: Shridhan Varadkar <shridhan98@gmail.com>

--- device.mk
+++ device.mk
@@ -157,14 +157,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DeviceDoze
 
-# Dirac
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.soundfx.dirac=true \
-    persist.audio.dirac.speaker=true \
-    persist.dirac.acs.controller=qem \
-    persist.dirac.acs.ignore_error=1 \
-    persist.dirac.acs.storeSettings=1
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
