1d23c5b610596ff9d9f21bf1d396f2df628d8598
danascape <danascape@gmail.com>
2022-11-17 14:49:25 +0530

    lavender: Drop unneeded TARGET_ENABLE_MEDIADRM_64
    Change-Id: I9c80a374847930216d908c1afa5c493f51ece0e6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,9 +90,6 @@ TARGET_USES_GRALLOC4 := true
 TARGET_USES_ION := true
 TARGET_SCREEN_DENSITY := 420
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
