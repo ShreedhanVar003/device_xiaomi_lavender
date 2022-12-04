9a019a8601ac3a3d9b3e88f09be4b98beee6d683
Shreedhan003 <shridhan98@gmail.com>
2022-12-04 16:08:08 +0530

    lavender: Drop duplicate displayservice

--- device.mk
+++ device.mk
@@ -180,7 +180,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    android.frameworks.displayservice@1.0 \
     vendor.display.config@2.0 \
     vendor.display.config@2.0_vendor
 
