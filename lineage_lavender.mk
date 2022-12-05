33fa35c494d5efd463864a7019d44304cacf1772
Shreedhan003 <shridhan98@gmail.com>
2022-12-05 19:48:35 +0530

    lavender: LineageOS bringup

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,22 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-
-# Official-ify
-VOLTAGE_BUILD_TYPE := OFFICIAL
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 2160
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := voltage_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_MODEL := Redmi Note 7/7S
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
